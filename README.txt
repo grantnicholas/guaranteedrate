2ded4b10954d732c30bbc1b276dc41a2534af5ce
Grant Nicholas <grantnicholas2015@u.northwestern.edu>
2016-11-08 13:18:57 -0600

    Updated .gitignore, README, and made package installable using setup.py

---
+++ README.txt
@@ -0,0 +1,5 @@
+GuaranteedRate personapi project:
+
+sample usage:
+`python personapi/cli/person_ingester.py -f personapi/test/testfiles/file-comma.txt -d ',' -t 1`
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='personapi',
+   version='1.0',
+   description='A module for ingesting, sorting, and returning person data',
+   author='Grant Nicholas',
+   author_email='grantnicholas2015{at}u.northwestern.edu',
+   packages=find_packages(),
+   install_requires=[]
+)
